33f0f655f43d8ef3fb377825a927a79c23835c3b
Ayende Rahien <ayende@ayende.com>
2013-11-04 12:01:18 +0000

    Ading one-to-many & many-to-one

--- NovCoure/Model/Building.cs
+++ NovCoure/Model/Building.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.Design;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
 using Iesi.Collections;
 
 namespace NovCoure.Model
@@ -8,5 +10,7 @@ namespace NovCoure.Model
 		public virtual int Id { get; set; } 
 		public virtual string ZipCode { get; set; }
 		public virtual string Name { get; set; }
+
+		public virtual ICollection<MaintenanceJob> Jobs { get; set; }
 	}
 }
--- NovCoure/Program.cs
+++ NovCoure/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using NHibernate.Cfg;
 using NHibernate.Dialect;
+using NHibernate.Linq;
 using NovCoure.Model;
 
 namespace NovCoure
@@ -15,10 +16,11 @@ namespace NovCoure
 			App_Start.NHibernateProfilerBootstrapper.PreStart();
 
 			var cfg = new Configuration();
+			
 			cfg.DataBaseIntegration(properties =>
 			{
 				properties.Dialect<MsSql2008Dialect>();
-				properties.SchemaAction = SchemaAutoAction.Update;
+				properties.SchemaAction = SchemaAutoAction.Recreate;
 				properties.ConnectionString = @"Data Source=.\SqlExpress;Initial Catalog=Jobs;Integrated Security=true";
 			});
 
@@ -29,15 +31,44 @@ namespace NovCoure
 			using (var session = sessionFactory.OpenSession())
 			using (var tx = session.BeginTransaction())
 			{
-				session.Save(new Building
+				var building = new Building
 				{
 					ZipCode = "ECA 123A",
-					Name = "107-110 Fleet St"
-				});
+					Name = "107-110 Fleet St",
+					Jobs = new HashSet<MaintenanceJob>()
+				};
+				var emp = new Employee
+				{
+					Name = "Prachett"
+				};
+				var maintenanceJob = new MaintenanceJob
+				{
+					Building = building,
+					At = DateTime.Today.AddDays(1),
+					By = new HashSet<Employee>{emp}
+				};
+				building.Jobs.Add(maintenanceJob);
+
+				session.Save(emp);
+				session.Save(building);
+				session.Save(maintenanceJob);
+
+				//var q = session.Query<MaintenanceJob>()
+				//	.ToList();
+
+				//foreach (var maintenanceJob in q)
+				//{
+				//	Console.WriteLine(maintenanceJob.Building.Name + " " + maintenanceJob.At);
+				//}
+
+
+				//session.Query<Building>()
+				//	.Where(x => x.Jobs.Count > 1)
+				//	.ToList();
+
 
 				tx.Commit();
 			}
-
 		}
 	}
 }
