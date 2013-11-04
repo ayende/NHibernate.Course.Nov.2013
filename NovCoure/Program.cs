1bd7bcc36652939d8fd17e0d4c528f83ece0e2a0
Ayende Rahien <ayende@ayende.com>
2013-11-04 09:47:59 +0000

    Setting up NHibernate

--- NovCoure/Program.cs
+++ NovCoure/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NovCoure.Model;
 
 namespace NovCoure
 {
@@ -10,6 +13,17 @@ namespace NovCoure
 		static void Main(string[] args)
 		{
 			App_Start.NHibernateProfilerBootstrapper.PreStart();
+
+			var cfg = new Configuration();
+			cfg.DataBaseIntegration(properties =>
+			{
+				properties.Dialect<MsSql2008Dialect>();
+				properties.ConnectionString = @"Data Source=.\SqlExpress;Initial Catalog=Jobs;Integrated Security=true";
+			});
+
+			cfg.AddAssembly(typeof (Building).Assembly);
+
+			var sessionFactory = cfg.BuildSessionFactory();
 
 
 		}
