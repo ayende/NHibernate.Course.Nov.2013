a6fae252df112ef1eb59794deba864aee669e00f
Ayende Rahien <ayende@ayende.com>
2013-11-04 09:35:23 +0000

    Initial commit

---
+++ NovCoure/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NovCoure
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
